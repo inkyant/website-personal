ad6728b781139c938eeffbe2570b833e37523bc2
Anthony Furman <80181074+inkyant@users.noreply.github.com>
2023-10-04 15:32:37 -0700

    fix line lengths

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,9 +8,9 @@ import Layout from "@components/layout";
 import Lines from "@components/home/lines";
 
 export const projects = [
-  {title: "test",   text: 'lorem ipsum dolor sit amet anoewuniuren iwniure niurn ijwdnf', path: "M13 0.5C-30.5 285.5 94.5 281.5 13 468.5"},
-  {title: "yeeee",  text: 'lorem ipsum dolor sit amet anoewuniuren iwniure niurn ijwdnf', path: "M13 0.5C12.8414 50.8751 -1.10819 139.319 11.5 204C33.991 319.38 94.7921 398.423 100.5 330.5C107.266 249.982 34.3602 289.008 6.50001 360.5C-12.9849 410.5 33.8799 434.784 13 465.5"},
-  {title: "mann",   text: 'lorem ipsum dolor sit amet anoewuniuren iwniure niurn ijwdnf', path: "M13 0.5C3.28773 54.7716 38.6635 117.5 25.5 150.5C-46.5 331 79.409 315.61 13 467"}
+  {title: "test",   text: 'lorem ipsum dolor sit amet anoewuniuren iwniure niurn ijwdnf', path: "M13 0.5C-30.5 285.5 94.5 281.5 13 470"},
+  {title: "yeeee",  text: 'lorem ipsum dolor sit amet anoewuniuren iwniure niurn ijwdnf', path: "M13 0.5C12.8414 50.8751 -1.10819 139.319 11.5 204C33.991 319.38 94.7921 398.423 100.5 330.5C107.266 249.982 34.3602 289.008 6.50001 360.5C-12.9849 410.5 33.8799 434.784 13 470"},
+  {title: "mann",   text: 'lorem ipsum dolor sit amet anoewuniuren iwniure niurn ijwdnf', path: "M13 0.5C3.28773 54.7716 38.6635 117.5 25.5 150.5C-46.5 331 79.409 315.61 13 470"}
 ]
 
 // configure the settings for animation
