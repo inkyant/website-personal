788829697e8c3e41ca8ce2664ee5d73e60919028
Anthony Furman <80181074+inkyant@users.noreply.github.com>
2023-10-05 11:39:23 -0700

    add text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,9 +9,32 @@ import Lines from "@components/home/lines";
 import IconsBackground from "@components/home/iconsBackground";
 
 export const projects = [
-  {title: "test",   text: 'lorem ipsum dolor sit amet anoewuniuren iwniure niurn ijwdnf', path: "M13 0.5C-30.5 285.5 94.5 281.5 13 470"},
-  {title: "yeeee",  text: 'lorem ipsum dolor sit amet anoewuniuren iwniure niurn ijwdnf', path: "M13 0.5C12.8414 50.8751 -1.10819 139.319 11.5 204C33.991 319.38 94.7921 398.423 100.5 330.5C107.266 249.982 34.3602 289.008 6.50001 360.5C-12.9849 410.5 33.8799 434.784 13 470"},
-  {title: "mann",   text: 'lorem ipsum dolor sit amet anoewuniuren iwniure niurn ijwdnf', path: "M13 0.5C3.28773 54.7716 38.6635 117.5 25.5 150.5C-46.5 331 79.409 315.61 13 470"}
+  {title: "Robotics",   
+    text: `I was the programming director for FRC team 972 Iron Claw Robotics, and with my contributions our team led the 
+    top 8 alliance in the world (we had previously never even made it to championships). I led projects for autonomous path following
+    on a swerve drivetrain, vision processing for robot position estimation, and nearly all programming on the team. I implemented industy
+    standard code processes to help more people contribute to the team by creating a tasklist and following PR procedures with automatic 
+    code tests and expert code reviews. I also created a curriculum to teach the new students various aspects of robotics, from electronics
+    to control theory to autonomous movement and networks. I was also a part of the Curriculum Council making decisions on how to manage the
+    50 student team.`, 
+    path: "M13 0.5C-30.5 285.5 94.5 281.5 13 470"},
+  {title: "Website",  
+    text: `I created this website using React. I also created a version using Hugo (very close to plain js/html/css). Right now, this 
+    website is still under development but I have implemented many complicated UI features without the use of any add on React libraries. 
+    Though using libraries would make development quicker and easier, it allowed me to better learn and demonstrate the skills needed 
+    for React development. I have had previous web development experience as a part of the Open Source Club, where in a small team we 
+    created a website to run a mass chess game between two schools. This involved using mongo.db databases as well as google OAuth.`,
+    path: "M13 0.5C12.8414 50.8751 -1.10819 139.319 11.5 204C33.991 319.38 94.7921 398.423 100.5 330.5C107.266 249.982 34.3602 289.008 6.50001 360.5C-12.9849 410.5 33.8799 434.784 13 470"},
+  {title: "Game Design",   
+    text: `I created and published modifications for the history game Civilization, which garnered over 5000 downloads. This involved 
+    creating lua scripts, using the  SQL database, and modifying the source DLL in C++. I had to consider aspects of game design and did
+    research into the history through books and articles. I also learned art asset creation techniques, like Photoshop and Blender.`,
+    path: "M13 0.5C3.28773 54.7716 38.6635 117.5 25.5 150.5C-46.5 331 79.409 315.61 13 470"},
+  {title: "AI",   
+    text: `With a friend we recreated the popular game Super Auto Pets in python. I then created a Deep Q Reinforcement Learning algortihm
+    using pytorch to try to beat the game. While this project is still in progress, it has successfully beat teams that grow exponentially
+    stronger with level. I hope to improve the simulation and tune the AI to create a better model.`,
+    path: "M13 0.5C-30.5 285.5 94.5 281.5 13 470"},
 ]
 
 // configure the settings for animation
