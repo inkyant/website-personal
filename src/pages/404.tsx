30a77b81dc646dc9c144ae5945afdd5fd844147c
Anthony Furman <80181074+inkyant@users.noreply.github.com>
2023-10-09 22:15:01 -0700

    add error page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,49 +1,23 @@
 import * as React from "react"
 import { Link, HeadFC, PageProps } from "gatsby"
+import Layout from "@components/layout"
+import IconsBackground from "@components/home/iconsBackground"
 
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+import { main } from "@styles/pages/404.module.scss"
 
 const NotFoundPage: React.FC<PageProps> = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
+    <Layout>
+      <IconsBackground height="100"></IconsBackground>
+      <div className={main}>
+          <p>Sorry 😔, we couldn't find what you were looking for.</p>
+          <br />
+          <Link to="/">Click to go home</Link>.
+      </div>
+    </Layout>
   )
 }
 
 export default NotFoundPage
 
-export const Head: HeadFC = () => <title>Not found</title>
+export { Head } from "@pages/index"
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -17,10 +17,5 @@ export default function About() {
     )
 }
 
-export const Head: HeadFC = () => 
-<>        
-  <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" />
-  <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap" rel="stylesheet" />
-  <title>Anthony Furman</title>
-</>
+
+export { Head } from "@pages/index"
