ad4643f2cf6c3a91ea161926b457d188ea3a01f1
Anthony Furman <80181074+inkyant@users.noreply.github.com>
2024-01-03 01:30:17 -0800

    move line paths to lines file

--- src/components/home/lines.tsx
+++ src/components/home/lines.tsx
@@ -2,7 +2,6 @@
 import React from "react";
 
 import Drawn from "@components/home/drawn";
-import { projects } from "@pages";
 import { DrawingHandle } from "./drawn";
 import onVisible from "@components/common/visible";
 
@@ -12,6 +11,14 @@ const ANGLE = Math.PI / 4
 const RADIUS = 20 // radius, ie length, of the arrow at the end
 const xArrow = RADIUS*Math.cos(ANGLE)
 const yArrow = RADIUS*Math.sin(ANGLE)
+
+const linePaths = [
+    "M13 0.5C-30.5 285.5 94.5 281.5 13 470",
+    "M13 0.5C12.8414 50.8751 -1.10819 139.319 11.5 204C33.991 319.38 94.7921 398.423 100.5 330.5C107.266 249.982 34.3602 289.008 6.50001 360.5C-12.9849 410.5 33.8799 434.784 13 470",
+    "M13 0.5C3.28773 54.7716 38.6635 117.5 25.5 150.5C-46.5 331 79.409 315.61 13 470",
+    "M13 0.5C-30.5 285.5 94.5 281.5 13 470",
+]
+
 
 export default function Lines() {
 
@@ -23,10 +30,10 @@ export default function Lines() {
     let isArrowPointDown = screenWidth < 600
 
     // make line refs: one for each project, and two more for start + end
-    const refs = Array(projects.length + 2).fill('').map(() => React.useRef<DrawingHandle>(null))
+    const refs = Array(linePaths.length + 2).fill('').map(() => React.useRef<DrawingHandle>(null))
 
-    const lines = projects.map((project, index) => 
-        <Drawn key={index} ref={refs[index+1]} drawingCallback={() => drawingCallback(index+1)} height="470" width="150" path={project.path}></Drawn>
+    const lines = linePaths.map((path, index) => 
+        <Drawn key={index} ref={refs[index+1]} drawingCallback={() => drawingCallback(index+1)} height="470" width="150" path={path}></Drawn>
     )
 
     // set up animation for first line and last line
