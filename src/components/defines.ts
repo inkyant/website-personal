293d7496dec51c3bb6616ebfa5778683687cc6ea
Anthony Furman <80181074+inkyant@users.noreply.github.com>
2024-09-11 16:47:45 -0700

    slightl lower animation area

--- src/components/defines.ts
+++ src/components/defines.ts
@@ -10,7 +10,7 @@ export let CIRCLE_LEFT_PX_NARROWSCREEN = 37
 // precision is the % step at which we animate, for example 0.01 draws 1% of the line for every 1% scrolled
 const PRECISION = 0.01
 // the percentage of screen height above the bottom of the viewport at which the line is "drawn" (just for the percent intersection, the line can go up)
-const ANIM_MARGIN_RATIO = 0.4
+const ANIM_MARGIN_RATIO = 0.35
 // the margin above the top of the screen that still counts as "intersecting", so that lines don't undraw
 export const ANIM_MARGIN_TOP = 200
 
