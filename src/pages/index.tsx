fe01e0cfe65a6b21e370b3519c8b809234fcbea7
Anthony Furman <80181074+inkyant@users.noreply.github.com>
2024-09-21 23:11:12 -0700

    iconsBackground just scales with section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -72,7 +72,7 @@ export default function Home() {
 
   return (
     <Layout>
-      <IconsBackground />
+      <IconsBackground sections={data.yaml.nodes.length} />
       <Welcome></Welcome>
 
       <div style={{position: "relative"}}>
